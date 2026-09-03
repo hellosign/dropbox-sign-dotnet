b70c69ff9372855f8139cfd6f70b5008b236fe04
mchatlas <mchatlas@dropbox.com>
2026-09-03 14:31:29 +0200

    Release 2.4.0

--- src/Dropbox.Sign/Model/SignatureRequestEditRequest.cs
+++ src/Dropbox.Sign/Model/SignatureRequestEditRequest.cs
@@ -65,9 +65,8 @@ namespace Dropbox.Sign.Model
         /// <param name="testMode">Whether this is a test, the signature request will not be legally binding if set to &#x60;true&#x60;. Defaults to &#x60;false&#x60;. (default to false).</param>
         /// <param name="title">The title you want to assign to the SignatureRequest..</param>
         /// <param name="useTextTags">Send with a value of &#x60;true&#x60; if you wish to enable [Text Tags](https://app.hellosign.com/api/textTagsWalkthrough#TextTagIntro) parsing in your document. Defaults to disabled, or &#x60;false&#x60;. (default to false).</param>
-        /// <param name="ignoreTextTagsExtractionErrors">Sent with a value of &#x60;true&#x60; to ignore the validation errors from text tags extraction. Defaults to &#x60;false&#x60;. (default to false).</param>
         /// <param name="expiresAt">When the signature request will expire. Unsigned signatures will be moved to the expired status, and no longer signable. See [Signature Request Expiration Date](https://developers.hellosign.com/docs/signature-request/expiration/) for details..</param>
-        public SignatureRequestEditRequest(List<System.IO.Stream> files = default(List<System.IO.Stream>), List<string> fileUrls = default(List<string>), List<SubSignatureRequestSigner> signers = default(List<SubSignatureRequestSigner>), List<SubSignatureRequestGroupedSigners> groupedSigners = default(List<SubSignatureRequestGroupedSigners>), bool allowDecline = false, bool allowReassign = false, List<SubAttachment> attachments = default(List<SubAttachment>), List<string> ccEmailAddresses = default(List<string>), string clientId = default(string), List<SubCustomField> customFields = default(List<SubCustomField>), SubFieldOptions fieldOptions = default(SubFieldOptions), List<SubFormFieldGroup> formFieldGroups = default(List<SubFormFieldGroup>), List<SubFormFieldRule> formFieldRules = default(List<SubFormFieldRule>), List<SubFormFieldsPerDocumentBase> formFieldsPerDocument = default(List<SubFormFieldsPerDocumentBase>), bool hideTextTags = false, bool isEid = false, string message = default(string), Dictionary<string, Object> metadata = default(Dictionary<string, Object>), SubSigningOptions signingOptions = default(SubSigningOptions), string signingRedirectUrl = default(string), string subject = default(string), bool testMode = false, string title = default(string), bool useTextTags = false, bool ignoreTextTagsExtractionErrors = false, int? expiresAt = default(int?))
+        public SignatureRequestEditRequest(List<System.IO.Stream> files = default(List<System.IO.Stream>), List<string> fileUrls = default(List<string>), List<SubSignatureRequestSigner> signers = default(List<SubSignatureRequestSigner>), List<SubSignatureRequestGroupedSigners> groupedSigners = default(List<SubSignatureRequestGroupedSigners>), bool allowDecline = false, bool allowReassign = false, List<SubAttachment> attachments = default(List<SubAttachment>), List<string> ccEmailAddresses = default(List<string>), string clientId = default(string), List<SubCustomField> customFields = default(List<SubCustomField>), SubFieldOptions fieldOptions = default(SubFieldOptions), List<SubFormFieldGroup> formFieldGroups = default(List<SubFormFieldGroup>), List<SubFormFieldRule> formFieldRules = default(List<SubFormFieldRule>), List<SubFormFieldsPerDocumentBase> formFieldsPerDocument = default(List<SubFormFieldsPerDocumentBase>), bool hideTextTags = false, bool isEid = false, string message = default(string), Dictionary<string, Object> metadata = default(Dictionary<string, Object>), SubSigningOptions signingOptions = default(SubSigningOptions), string signingRedirectUrl = default(string), string subject = default(string), bool testMode = false, string title = default(string), bool useTextTags = false, int? expiresAt = default(int?))
         {
 
             this.Files = files;
@@ -94,7 +93,6 @@ namespace Dropbox.Sign.Model
             this.TestMode = testMode;
             this.Title = title;
             this.UseTextTags = useTextTags;
-            this.IgnoreTextTagsExtractionErrors = ignoreTextTagsExtractionErrors;
             this.ExpiresAt = expiresAt;
         }
 
@@ -281,13 +279,6 @@ namespace Dropbox.Sign.Model
         public bool UseTextTags { get; set; }
 
         /// <summary>
-        /// Sent with a value of &#x60;true&#x60; to ignore the validation errors from text tags extraction. Defaults to &#x60;false&#x60;.
-        /// </summary>
-        /// <value>Sent with a value of &#x60;true&#x60; to ignore the validation errors from text tags extraction. Defaults to &#x60;false&#x60;.</value>
-        [DataMember(Name = "ignore_text_tags_extraction_errors", EmitDefaultValue = true)]
-        public bool IgnoreTextTagsExtractionErrors { get; set; }
-
-        /// <summary>
         /// When the signature request will expire. Unsigned signatures will be moved to the expired status, and no longer signable. See [Signature Request Expiration Date](https://developers.hellosign.com/docs/signature-request/expiration/) for details.
         /// </summary>
         /// <value>When the signature request will expire. Unsigned signatures will be moved to the expired status, and no longer signable. See [Signature Request Expiration Date](https://developers.hellosign.com/docs/signature-request/expiration/) for details.</value>
@@ -326,7 +317,6 @@ namespace Dropbox.Sign.Model
             sb.Append("  TestMode: ").Append(TestMode).Append("\n");
             sb.Append("  Title: ").Append(Title).Append("\n");
             sb.Append("  UseTextTags: ").Append(UseTextTags).Append("\n");
-            sb.Append("  IgnoreTextTagsExtractionErrors: ").Append(IgnoreTextTagsExtractionErrors).Append("\n");
             sb.Append("  ExpiresAt: ").Append(ExpiresAt).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
@@ -489,10 +479,6 @@ namespace Dropbox.Sign.Model
                     this.UseTextTags.Equals(input.UseTextTags)
                 ) &&
                 (
-                    this.IgnoreTextTagsExtractionErrors == input.IgnoreTextTagsExtractionErrors ||
-                    this.IgnoreTextTagsExtractionErrors.Equals(input.IgnoreTextTagsExtractionErrors)
-                ) &&
-                (
                     this.ExpiresAt == input.ExpiresAt ||
                     (this.ExpiresAt != null &&
                     this.ExpiresAt.Equals(input.ExpiresAt))
@@ -586,7 +572,6 @@ namespace Dropbox.Sign.Model
                     hashCode = (hashCode * 59) + this.Title.GetHashCode();
                 }
                 hashCode = (hashCode * 59) + this.UseTextTags.GetHashCode();
-                hashCode = (hashCode * 59) + this.IgnoreTextTagsExtractionErrors.GetHashCode();
                 if (this.ExpiresAt != null)
                 {
                     hashCode = (hashCode * 59) + this.ExpiresAt.GetHashCode();
@@ -792,13 +777,6 @@ namespace Dropbox.Sign.Model
                 Property = "UseTextTags",
                 Type = "bool",
                 Value = UseTextTags,
-            });
-            types.Add(new OpenApiType()
-            {
-                Name = "ignore_text_tags_extraction_errors",
-                Property = "IgnoreTextTagsExtractionErrors",
-                Type = "bool",
-                Value = IgnoreTextTagsExtractionErrors,
             });
             types.Add(new OpenApiType()
             {
--- src/Dropbox.Sign/Model/TemplateCreateEmbeddedDraftRequest.cs
+++ src/Dropbox.Sign/Model/TemplateCreateEmbeddedDraftRequest.cs
@@ -66,8 +66,7 @@ namespace Dropbox.Sign.Model
         /// <param name="testMode">Whether this is a test, the signature request created from this draft will not be legally binding if set to &#x60;true&#x60;. Defaults to &#x60;false&#x60;. (default to false).</param>
         /// <param name="title">The title you want to assign to the SignatureRequest..</param>
         /// <param name="usePreexistingFields">Enable the detection of predefined PDF fields by setting the &#x60;use_preexisting_fields&#x60; to &#x60;true&#x60; (defaults to disabled, or &#x60;false&#x60;). (default to false).</param>
-        /// <param name="ignoreTextTagsExtractionErrors">Sent with a value of &#x60;true&#x60; to ignore the validation errors from text tags extraction. Defaults to &#x60;false&#x60;. (default to false).</param>
-        public TemplateCreateEmbeddedDraftRequest(List<System.IO.Stream> files = default(List<System.IO.Stream>), List<string> fileUrls = default(List<string>), bool allowCcs = true, bool allowReassign = false, List<SubAttachment> attachments = default(List<SubAttachment>), List<string> ccRoles = default(List<string>), string clientId = default(string), SubEditorOptions editorOptions = default(SubEditorOptions), SubFieldOptions fieldOptions = default(SubFieldOptions), bool forceSignerRoles = false, bool forceSubjectMessage = false, List<SubFormFieldGroup> formFieldGroups = default(List<SubFormFieldGroup>), List<SubFormFieldRule> formFieldRules = default(List<SubFormFieldRule>), List<SubFormFieldsPerDocumentBase> formFieldsPerDocument = default(List<SubFormFieldsPerDocumentBase>), List<SubMergeField> mergeFields = default(List<SubMergeField>), string message = default(string), Dictionary<string, Object> metadata = default(Dictionary<string, Object>), bool showPreview = false, bool showProgressStepper = true, List<SubTemplateRole> signerRoles = default(List<SubTemplateRole>), bool skipMeNow = false, string subject = default(string), bool testMode = false, string title = default(string), bool usePreexistingFields = false, bool ignoreTextTagsExtractionErrors = false)
+        public TemplateCreateEmbeddedDraftRequest(List<System.IO.Stream> files = default(List<System.IO.Stream>), List<string> fileUrls = default(List<string>), bool allowCcs = true, bool allowReassign = false, List<SubAttachment> attachments = default(List<SubAttachment>), List<string> ccRoles = default(List<string>), string clientId = default(string), SubEditorOptions editorOptions = default(SubEditorOptions), SubFieldOptions fieldOptions = default(SubFieldOptions), bool forceSignerRoles = false, bool forceSubjectMessage = false, List<SubFormFieldGroup> formFieldGroups = default(List<SubFormFieldGroup>), List<SubFormFieldRule> formFieldRules = default(List<SubFormFieldRule>), List<SubFormFieldsPerDocumentBase> formFieldsPerDocument = default(List<SubFormFieldsPerDocumentBase>), List<SubMergeField> mergeFields = default(List<SubMergeField>), string message = default(string), Dictionary<string, Object> metadata = default(Dictionary<string, Object>), bool showPreview = false, bool showProgressStepper = true, List<SubTemplateRole> signerRoles = default(List<SubTemplateRole>), bool skipMeNow = false, string subject = default(string), bool testMode = false, string title = default(string), bool usePreexistingFields = false)
         {
 
             // to ensure "clientId" is required (not null)
@@ -100,7 +99,6 @@ namespace Dropbox.Sign.Model
             this.TestMode = testMode;
             this.Title = title;
             this.UsePreexistingFields = usePreexistingFields;
-            this.IgnoreTextTagsExtractionErrors = ignoreTextTagsExtractionErrors;
         }
 
         /// <summary>
@@ -293,13 +291,6 @@ namespace Dropbox.Sign.Model
         public bool UsePreexistingFields { get; set; }
 
         /// <summary>
-        /// Sent with a value of &#x60;true&#x60; to ignore the validation errors from text tags extraction. Defaults to &#x60;false&#x60;.
-        /// </summary>
-        /// <value>Sent with a value of &#x60;true&#x60; to ignore the validation errors from text tags extraction. Defaults to &#x60;false&#x60;.</value>
-        [DataMember(Name = "ignore_text_tags_extraction_errors", EmitDefaultValue = true)]
-        public bool IgnoreTextTagsExtractionErrors { get; set; }
-
-        /// <summary>
         /// Returns the string presentation of the object
         /// </summary>
         /// <returns>String presentation of the object</returns>
@@ -332,7 +323,6 @@ namespace Dropbox.Sign.Model
             sb.Append("  TestMode: ").Append(TestMode).Append("\n");
             sb.Append("  Title: ").Append(Title).Append("\n");
             sb.Append("  UsePreexistingFields: ").Append(UsePreexistingFields).Append("\n");
-            sb.Append("  IgnoreTextTagsExtractionErrors: ").Append(IgnoreTextTagsExtractionErrors).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
         }
@@ -493,10 +483,6 @@ namespace Dropbox.Sign.Model
                 (
                     this.UsePreexistingFields == input.UsePreexistingFields ||
                     this.UsePreexistingFields.Equals(input.UsePreexistingFields)
-                ) &&
-                (
-                    this.IgnoreTextTagsExtractionErrors == input.IgnoreTextTagsExtractionErrors ||
-                    this.IgnoreTextTagsExtractionErrors.Equals(input.IgnoreTextTagsExtractionErrors)
                 );
         }
 
@@ -582,7 +568,6 @@ namespace Dropbox.Sign.Model
                     hashCode = (hashCode * 59) + this.Title.GetHashCode();
                 }
                 hashCode = (hashCode * 59) + this.UsePreexistingFields.GetHashCode();
-                hashCode = (hashCode * 59) + this.IgnoreTextTagsExtractionErrors.GetHashCode();
                 return hashCode;
             }
         }
@@ -785,13 +770,6 @@ namespace Dropbox.Sign.Model
                 Property = "UsePreexistingFields",
                 Type = "bool",
                 Value = UsePreexistingFields,
-            });
-            types.Add(new OpenApiType()
-            {
-                Name = "ignore_text_tags_extraction_errors",
-                Property = "IgnoreTextTagsExtractionErrors",
-                Type = "bool",
-                Value = IgnoreTextTagsExtractionErrors,
             });
 
             return types;
